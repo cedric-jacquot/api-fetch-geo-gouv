e21791db67f346df0d136e2d74a0cc07ae961c16
Cédric <cedric.jacquot54@gmail.com>
2021-04-30 22:21:01 +0200

    recherche de ville par le code postal

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// exemple avec la ville "gondreville"
+// https://geo.api.gouv.fr/communes?codePostal=54840&fields=,nom,code,codesPostaux,centre,surface,codeDepartement,departement,codeRegion,region,population&format=json&geometry=centre
+
+const elementInputZipcode = document.getElementById('zipcode');
+const elementSelectCity = document.getElementById('city');
+
+const elementP = document.getElementById('cp');
+const elementAPILink = document.getElementById('api-link');
+
+let fetOptions =
+{
+    mode: 'cors',
+}
+
+function api(event) {
+    event.preventDefault();
+
+    let zipcode = elementInputZipcode.value;
+
+    if (zipcode.length == 0) {
+        alert("Le code postal est vide");
+    } else {
+        let APILink = "https://geo.api.gouv.fr/communes?codePostal=" + zipcode + "&fields=,nom,code,codesPostaux,centre,surface,codeDepartement,departement,codeRegion,region,population&format=json&geometry=centre";
+
+        fetch(APILink, fetOptions)
+            .then(convertFromJson)
+            .then(displayCities);
+    }    
+}
+
+function convertFromJson(response) {
+    return data = response.json();
+}
+
+function displayCities (cities) {
+    console.log(cities);
+    for (let city in cities) {
+        console.log(cities[city].nom);
+        let option = document.createElement('option');
+        option.text = cities[city].nom + " (code insee : " + cities[city].code + ")";
+        option.value = cities[city].code;
+        elementSelectCity.add(option);
+    }
+}
+
+document.getElementById('validate').addEventListener('click', api);
